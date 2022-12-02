fd8b78ad1f1a27ea0c45fe11d19c641a5a5f6cd0
Jupil Hwang <jupil.hwang@gmail.com>
2022-12-02 13:46:23 +0900

    remove metrics server

--- shared/k8s-join.sh
+++ shared/k8s-join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.5:6443 --token 5jfuj7.fizfx5cjm2xkpv0n --discovery-token-ca-cert-hash sha256:355a0ea5815c5324130ad88c2766bdf14d472b4ac6431439ce31af26048fd738 
+kubeadm join 192.168.56.5:6443 --token nlaqot.z95hcj11iseqmegz --discovery-token-ca-cert-hash sha256:57ceb311f3fec7be3b8a589244af29eebe297770bd721078ef3d658375e99184 
